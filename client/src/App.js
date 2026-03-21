31b8b5b76e92ca682a692a44d18eb2bc46a1e078
mazizullah01 <m.azizullah420@gmail.com>
2026-03-21 23:24:08 +0500

    feat: setup app layout using styled-components and theme provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,39 @@
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Theme";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost"
+
+const Container = styled.div`
+  width: 100%;
+  height:100%;
+  display: flex;
+  background: ${({ theme }) => theme.bg};
+  clor; ${({ theme })=> theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transtion: all 0.2s ease;
+`;
+
+const Wrapper = styled.div`
+  height: 100;
+  position: relative;
+  display: flex;
+  flex-direction: coloumn;
+  justify-content: space-between;
+  flex: 3;
+`;
+
 function App() {
-  return <div>Hello</div>;
+  return ( 
+  <ThemeProvider theme={darkTheme}>
+    <Container>
+      <Wrapper>
+      <Home/>
+      <CreatePost/>
+      </Wrapper>
+    </Container>
+  </ThemeProvider>
+  );
 }
 
 export default App;
