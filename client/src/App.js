50eea58019d0d84cfa0299aaa1f995aff9d0a14f
mazizullah01 <m.azizullah420@gmail.com>
2026-03-23 22:35:59 +0500

    fix layout and navbar alignment

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ const Container = styled.div`
   width: 100%;
   height:100%;
   display: flex;
+  flex-direction: column; 
   background: ${({ theme }) => theme.bg};
   color: ${({ theme })=> theme.text_primary};
   overflow-x: hidden;
