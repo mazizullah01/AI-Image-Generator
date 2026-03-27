f25bd309089ce20ef2a7013388cc3d84951a2214
mazizullah01 <m.azizullah420@gmail.com>
2026-03-27 16:21:44 +0500

    feat: Add search bar component with styled-components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,51 @@
 import React from "react";
 import styled from "styled-components";
+import SearchBar from "../components/SearchBar";
 
 const Container = styled.div`
-height: 100%
-overflow-y: scroll;
+  height: 100%
+  overflow-y: scroll;
+  background: ${({ theme }) => theme.bg};
+  padding: 30px 30px;
+  padding-bottom: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+
+  @media (max-width: 768) {
+    padding: 6px 10px
+  }
+`;
+
+const Headline = styled.div`
+  font-size: 34px;
+  font-weight: 500;
+  color: ${({ theme}) => theme.text_primary};
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  @media (max-width: 600px) {
+  font-sixe: 22px;
+  }
+`;
+
+const Span = styled.div`
+  font-size: 30px;
+  font-weight: 800;
+  color: ${({ theme}) => theme.secondary};
 `;
 
 const  Home = () => {
   return (
-  <Container>Home</Container>
+  <Container>
+    <Headline>
+      Explore popular posts in the community!
+      <Span>⦿ Generated with AI ⦿</Span>
+    </Headline>
+    <SearchBar />
+  </Container>
   );
 }
 
