f9faa8e93c78389b89a08a2d2845ba26c7d3a8a8
mazizullah01 <m.azizullah420@gmail.com>
2026-03-29 17:46:52 +0500

    feat: Add ImageCard component

---
+++ client/src/components/ImageCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components"
+import { LazyLoadImage } from "react-lazy-load-image-component"; 
+
+const Card = styled.div`
+
+`;
+
+const ImageCard = () => {
+    return (
+        <Card>
+            <LazyLoadImage src="https://wwhttps://www.google.com/imgres?q=images&imgurl=https%3A%2F%2Fimg.freepik.com%2Ffree-photo%2Flavender-field-sunset-near-valensole_268835-3910.jpg%3Fsemt%3Dais_incoming%26w%3D740%26q%3D80&imgrefurl=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Flandscape&docid=rckQF1DyREyoYM&tbnid=5FRPWIYpd4hbrM&vet=12ahUKEwiy_JyUkMWTAxW5NfsDHaZpJIoQnPAOegQIGxAB..i&w=740&h=493&hcb=2&ved=2ahUKEwiy_JyUkMWTAxW5NfsDHaZpJIoQnPAOegQIGxABw.google.com/imgres?q=images&imgurl=https%3A%2F%2Fimg.freepik.com%2Ffree-photo%2Flavender-field-sunset-near-valensole_268835-3910.jpg%3Fsemt%3Dais_incoming%26w%3D740%26q%3D80&imgrefurl=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Flandscape&docid=rckQF1DyREyoYM&tbnid=5FRPWIYpd4hbrM&vet=12ahUKEwiy_JyUkMWTAxW5NfsDHaZpJIoQnPAOegQIGxAB..i&w=740&h=493&hcb=2&ved=2ahUKEwiy_JyUkMWTAxW5NfsDHaZpJIoQnPAOegQIGxAB"/>
+        </Card>
+    )
+}
+
+export default ImageCard;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import SearchBar from "../components/SearchBar";
+import ImageCard from "../components/ImageCard";
 
 const Container = styled.div`
-  height: 100%
+  height: 100%;
   overflow-y: scroll;
   background: ${({ theme }) => theme.bg};
   padding: 30px 30px;
@@ -13,8 +14,8 @@ const Container = styled.div`
   align-items: center;
   gap: 20px;
 
-  @media (max-width: 768) {
-    padding: 6px 10px
+  @media (max-width: 768px) {
+    padding: 6px 10px;
   }
 `;
 
@@ -27,14 +28,40 @@ const Headline = styled.div`
   flex-direction: column;
 
   @media (max-width: 600px) {
-  font-sixe: 22px;
+  font-size: 22px;
   }
 `;
 
 const Span = styled.div`
-  font-size: 30px;
+  font-size: 20px;
   font-weight: 800;
   color: ${({ theme}) => theme.secondary};
+
+  @media (max-width: 600px) {
+  font-size: 20px;
+  }
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  max-width: 1400px;
+  padding: 32px 0px;
+  display: flex;
+  justify-content: center;
+`;
+
+const CardWrapper = styled.div`
+  display: grid;
+  gap: 20px;
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+    @media (min-width: 640px) and (max-width: 1199px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+     @media (max-width: 639px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `;
 
 const  Home = () => {
@@ -45,6 +72,11 @@ const  Home = () => {
       <Span>⦿ Generated with AI ⦿</Span>
     </Headline>
     <SearchBar />
+    <Wrapper>
+      <CardWrapper>
+        <ImageCard />
+      </CardWrapper>
+    </Wrapper>
   </Container>
   );
 }
