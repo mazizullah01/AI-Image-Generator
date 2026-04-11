73335e0f2918b48a0fb473dbff509e8e450af2bb
mazizullah01 <m.azizullah420@gmail.com>
2026-04-11 20:29:27 +0500

    feat: setup express server with MongoDB connection and error handler

---
+++ server/index.js
@@ -0,0 +1,52 @@
+import cors from "cors";
+import express from 'express'
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+
+dotenv.config;
+
+const app = express();
+app.use(cors());
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({extended: true}));
+//Error handler
+app.use((err, req,res,next) => {
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong!";
+    return res.status(status).json({
+        success: false,
+        status,
+        message,
+    });
+});
+
+//Default get
+app.get("/", async (req, res) => {
+    res.status(200).json({
+        message:"Hello GFG Developer!",
+    });
+});
+
+//Function to connect mongodb
+const connectDB = () => {
+    mongoose.set("strictQuery", true);
+    mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => {
+        console.error("Failed to connect toDB");
+        console.error(err);
+    });
+};
+
+//Function to sstart the server
+const startServer = async () => {
+    try{
+        connectDB();
+        app.listen(9090, () => console.log("Server started on port 9090"));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+startServer();
