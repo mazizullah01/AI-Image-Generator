b166aafdb063c15ab39a19f381a377e951f144a7
mazizullah01 <m.azizullah420@gmail.com>
2026-05-09 14:14:09 +0500

    Fix ESLint warning

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import GenerateImageForm from "../components/GenerateImageForm";
 import GeneratedImageCard from "../components/GeneratedImageCard";
-import { use } from "react";
 
 const Container = styled.div`
   height: 100%;
